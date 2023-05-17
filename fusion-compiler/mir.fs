a1dae930f36c918e010f73461293416f65c5a6ea
Julian Hartl <julian.hartl.dev@gmail.com>
2023-05-17 17:21:14 +0200

    Adjust hello world example

--- fusion-compiler/mir.fs
+++ fusion-compiler/mir.fs
@@ -1,10 +1,7 @@
 func extern write(fd: i64, buf: *char, len: i64) -> i64
-func fib(n: i64) -> i64 {
-    if n <= 1 {
-        return n
-    }
-    return fib(n - 1) + fib(n - 2)
-}
+
 func main -> i64 {
-    return fib(8)
+    let buf = "Hello, world!\n"
+    write(1, buf, 14)
+    return 0
 }
