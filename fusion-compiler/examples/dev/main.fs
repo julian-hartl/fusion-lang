bb437bd7ea61e29b9228fd82870eae64f3a10591
Julian Hartl <julian.hartl.dev@gmail.com>
2023-05-29 21:12:39 +0200

    add struct field access

--- fusion-compiler/examples/dev/main.fs
+++ fusion-compiler/examples/dev/main.fs
@@ -1,10 +1,15 @@
-
+struct Program {
+    text: *char,
+}
 
 func main -> i64 {
     let msg = "Hello, world!"
     std::io::println(msg)
     set_new_msg(&mut msg)
     std::io::println(msg)
+    let mut program = Program { text: msg }
+    program.text = "Goodbye, world!"
+    std::io::println(program.text)
     return 0
 }
 
