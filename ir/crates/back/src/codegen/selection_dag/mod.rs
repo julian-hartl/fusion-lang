a220bd2f07d122c8ef9774d745d4d244e9574346
Julian Hartl <julian.hartl.dev@gmail.com>
2024-03-28 13:50:12 +0100

    Remove phi pseudo instruction

--- ir/crates/back/src/codegen/selection_dag/mod.rs
+++ ir/crates/back/src/codegen/selection_dag/mod.rs
@@ -242,7 +242,7 @@ pub enum PseudoOp<TM: TargetMachine> {
     Def(VReg),
     Copy(Register<TM>, Register<TM>),
     Ret(Option<Operand<TM>>),
-    Phi(Register<TM>, Vec<Register<TM>>),
+    Phi(Register<TM>, Vec<(Register<TM>, BasicBlockId)>),
 }
 
 impl<TM: TargetMachine> PseudoOp<TM> {
@@ -262,7 +262,9 @@ impl<TM: TargetMachine> PseudoOp<TM> {
                 Some(Operand::Reg(reg)) => smallvec![*reg],
                 _ => smallvec![],
             },
-            Self::Phi(_, regs) => regs.clone().into(),
+            Self::Phi(_, regs) => regs.iter().map(
+                |(reg, _)| *reg
+            ).collect(),
             Self::Def(_) => smallvec![],
         }
     }
